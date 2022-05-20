ee3c0e36acd79fceefc2952ba53fcbd5a5b812fd
Loran Abbas <loranabbas@abv.bg>
2022-05-20 17:19:11 +0300

    Add project files.

---
+++ ConsoleApp69/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp69
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                if (input[0] == 1)
+                {
+                    stack.Push(input[1]);
+                }
+                else if (input[0] == 2)
+                {
+                    stack.Pop();
+                }
+                else if (input[0] == 3)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(stack.Max());
+                }
+                else if (input[0] == 4)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(stack.Min());
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
